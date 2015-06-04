ed18a9d4604ca3f0f7d30f8a988c25c6f5841261
Ricky Su <ricky@ez2.us>
2015-06-04 09:53:30 +0800

    update travis compile config

--- .travis.scripts/compile.sh
+++ .travis.scripts/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo apt-get install gdb
 phpize
-./configure --quiet
-make all install
-echo "extension=mongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+./configure
+make all 
+sudo install
+sudo echo "extension=mongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
