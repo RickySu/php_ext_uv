590ab30fab9a8fabb3390b302f404e72b7c29141
Ricky Su <ricky@ez2.us>
2015-06-12 10:13:48 +0800

    fix zts build fail

--- .travis.scripts/compile.sh
+++ .travis.scripts/compile.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 sudo apt-get -y install check pkg-config
+thirdparty="`pwd`/thirdparty"
+
+cd "$thirdparty/r3" && \
+./autogen.sh && \
+CFLAGS="-O2 -fPIC" ./configure --prefix="$thirdparty/build" && make clean install
+
+cd "$thirdparty/libuv" && \
+./autogen.sh && \
+CFLAGS="-O2 -fPIC" ./configure --prefix="$thirdparty/build" && make clean install
+
 phpize
 ./configure
 make all 
 sudo install
-sudo echo "extension=mongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+sudo echo "extension=php_ext_uv.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
