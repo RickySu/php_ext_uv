5be5aac2bef01be2734af5665e832cfe60db9da7
Ricky Su <ricky@ez2.us>
2017-02-17 09:58:20 +0800

    add cpuinfo

--- src/uv_util.c
+++ src/uv_util.c
@@ -24,3 +24,22 @@ PHP_METHOD(UVUtil, errorMessage) {
     RETURN_STRING(uv_strerror(err));
 }
 
+PHP_METHOD(UVUtil, cpuinfo) {
+    zval retval, times;
+    uv_cpu_info_t *info;
+    int cpu_count;
+    uv_cpu_info(&info, &cpu_count);
+    array_init(&retval);
+    array_init(&times);
+    add_assoc_long(&times, "user", info->cpu_times.user);
+    add_assoc_long(&times, "nice", info->cpu_times.nice);
+    add_assoc_long(&times, "sys", info->cpu_times.sys);
+    add_assoc_long(&times, "idle", info->cpu_times.idle);
+    add_assoc_long(&times, "irq", info->cpu_times.irq);
+    add_assoc_long(&retval, "count", cpu_count);
+    add_assoc_string(&retval, "model", info->model);
+    add_assoc_long(&retval, "speed", info->speed);
+    add_assoc_zval(&retval, "times", &times);
+    uv_free_cpu_info(info, cpu_count);
+    RETURN_ZVAL(&retval, 0, 0);
+}
--- src/uv_util.h
+++ src/uv_util.h
@@ -12,12 +12,14 @@ PHP_METHOD(UVUtil, version);
 PHP_METHOD(UVUtil, versionString);
 PHP_METHOD(UVUtil, errorMessage);
 PHP_METHOD(UVUtil, __construct);
+PHP_METHOD(UVUtil, cpuinfo);
 
 DECLARE_FUNCTION_ENTRY(UVUtil) = {
     PHP_ME(UVUtil, __construct, NULL, ZEND_ACC_PRIVATE | ZEND_ACC_FINAL)
     PHP_ME(UVUtil, errorMessage, ARGINFO(UVUtil, errorMessage), ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
     PHP_ME(UVUtil, version, NULL, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
     PHP_ME(UVUtil, versionString, NULL, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
+    PHP_ME(UVUtil, cpuinfo, NULL, ZEND_ACC_PUBLIC | ZEND_ACC_STATIC)
     PHP_FE_END
 };
 
